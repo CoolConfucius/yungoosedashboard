15740a711be7609a3be5d7cb66e6f5a287e20cc4
Benjamin Huang <coolconfucius@yahoo.com>
2016-12-07 12:33:42 -0800

    routes module

--- index.js
+++ index.js
@@ -36,83 +36,8 @@ app.set('views', path.join(__dirname, './client/views'));
 // Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
 
-
-
-app.get('/', function(req, res) {
-  Yungoose.find({}, function(err, yungooses){
-    res.render('index', {yungooses})
-  })
-})
-
-app.get('/yungooses/new', function(req, res) {  
-  res.render('new')
-})
-
-app.get('/yungooses/:id', function(req, res) {
-  console.log("ID!", req.params.id);
-  Yungoose.findOne({_id: req.params.id}, function(err, yungoose){
-    res.render('detail', {yungoose})
-  })
-})
-
-
-
-
-
-app.post('/yungooses', function(req, res) {
-  console.log("POST DATA", req.body);
-
-  var yungoose = new Yungoose({
-    name: req.body.name, age: req.body.age
-  });
-  yungoose.save(function(err) {
-    if(err) {
-      console.log('something went wrong');
-    } else { 
-      console.log('successfully added a yungoose!');
-      res.redirect('/');
-    }
-  })
-})
-
-app.get('/yungooses/edit/:id', function(req, res) {  
-  Yungoose.findOne({_id: req.params.id}, function(err, yungoose){
-    res.render('edit', {yungoose})
-  })
-})
-
-app.post('/yungooses/destroy/:id', function(req, res) {
-  console.log("POST DATA", req.body);
-  // ...delete 1 record by a certain key/vaue.
-  Yungoose.remove({_id: req.params.id}, function(err){
-   // This code will run when the DB has attempted to remove all matching records to {_id: 'insert record unique id here'}
-   if(err) {
-      console.log('something went wrong');
-    } else { 
-      console.log('successfully removed a yungoose!');
-      res.redirect('/');
-    }
-  })
-})
-
-app.post('/yungooses/:id', function(req, res) {
-  console.log("POST DATA", req.body);
-  Yungoose.findOne({_id: req.params.id}, function(err, yungoose){
-    console.log("yungoose", yungoose);
-    yungoose.name = req.body.name || yungoose.name;
-    yungoose.age = req.body.age || yungoose.age;
-    yungoose.save(function(err) {
-      if(err) {
-        console.log('something went wrong');
-      } else { 
-        console.log('successfully edited a yungoose!');
-        res.redirect('/');
-      }
-    })
-  })
-})
-
-
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
 
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
